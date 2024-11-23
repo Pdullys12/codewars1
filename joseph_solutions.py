6d1edd8bed94712af0b213469c4c123e907de4f7
Pdullys12 <dullyspaulynis@gmail.com>
2024-11-23 10:08:01 -0500

    solution about function

---
+++ joseph_solutions.py
@@ -0,0 +1,21 @@
+# Function to evaluate if the number is even or odd
+def even_or_odd(number):
+   if number % 2 == 0:
+    return "Even"
+   else:
+        return "Odd"
+
+# Function to convert integer to string        
+def number_to_string(num): 
+    return str(num)
+    
+# Function to evaluate total count of vowels in provided string
+def get_count(sentence):
+    vowels = "aeiou"
+    
+    count = 0
+    for alphabet in sentence :
+        if alphabet in vowels :
+            count = count + 1
+            
+    return count
